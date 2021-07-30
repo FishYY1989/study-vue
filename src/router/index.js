4b13dccab890835ee0ce6f4aef1b39d3962b2fc9
yuyang <yuyang@ihealthlabs.com.cn>
2021-07-30 16:48:29 +0800

    路由守卫-全局守卫

--- src/router/index.js
+++ src/router/index.js
@@ -12,19 +12,27 @@ const routes = [
     component: Home
   },
   {
+    path: '/login',
+    name: 'login',
+    component: () => import('../views/Login.vue')
+  },
+  {
     path: '/admin',
     name: 'admin',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/Admin.vue'),
-    children:[
+    children: [
       {
         path: '/admin/course/:name',
         name: 'detail',
         component: () => import('../views/Detail.vue')
       }
-    ]
+    ],
+    meta: {
+      auth: true
+    }
   },
   {
     path: '/course/:name',
@@ -43,4 +51,19 @@ const router = new VueRouter({
   routes
 })
 
+//全局守卫
+router.beforeEach((to, from, next) => {
+  //判断路由是否需要守卫
+  //meta数据的方式
+  if (to.meta.auth) {
+    //是否登录
+    if (window.isLogin) {
+      next()
+    } else {
+      next('/login?redirect=' + to.fullPath)
+    }
+  } else {
+    next()
+  }
+})
 export default router
