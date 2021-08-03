23eae3a9b4be8d0448c1a44202342586f3e9fcce
yuyang <yuyang@ihealthlabs.com.cn>
2021-08-03 22:09:14 +0800

    12-2/3 vuex基础和模块化

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -9,36 +9,47 @@
 export default {
   methods: {
     login() {
-      window.isLogin = true;
-      //动态添加路由
-      this.$router.addRoutes([
-        {
-          path: "/admin",
-          name: "admin",
-          // route level code-splitting
-          // this generates a separate chunk (about.[hash].js) for this route
-          // which is lazy-loaded when the route is visited.
-          component: () =>
-            import(/* webpackChunkName: "about" */ "../views/Admin.vue"),
-          children: [
+      // window.isLogin = true;
+      //提交mutation变更状态
+      // this.$store.commit('user/login')
+      //派发动作，触发actions
+      this.$store
+        .dispatch("user/login", "admin")
+        .then(() => {
+          //动态添加路由
+          this.$router.addRoutes([
             {
-              path: "/admin/course/:name",
-              name: "detail",
-              component: () => import("../views/Detail.vue"),
+              path: "/admin",
+              name: "admin",
+              // route level code-splitting
+              // this generates a separate chunk (about.[hash].js) for this route
+              // which is lazy-loaded when the route is visited.
+              component: () =>
+                import(/* webpackChunkName: "about" */ "../views/Admin.vue"),
+              children: [
+                {
+                  path: "/admin/course/:name",
+                  name: "detail",
+                  component: () => import("../views/Detail.vue"),
+                },
+              ],
             },
-          ],
-        },
-      ]);
-      this.$router.push(this.$route.query.redirect);
+          ]);
+          this.$router.push(this.$route.query.redirect);
+        })
+        .catch(() => {
+          alert("用户名或密码错误，请重试");
+        });
     },
     logout() {
-      window.isLogin = false;
+      // window.isLogin = false;
+      this.$store.commit("user/logout");
       this.$router.push("/");
     },
   },
   computed: {
     isLogin() {
-      return window.isLogin;
+      return this.$store.state.user.isLogin;
     },
   },
 };
